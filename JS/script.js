03c7f4b23f7d7fd65f08434e7a06a92249f9d049
Vincent Uchenna <viccystarks9878@yahoo.com>
2023-02-03 00:40:10 +0200

    Commented the homepage loader

--- JS/script.js
+++ JS/script.js
@@ -67,12 +67,12 @@ document.querySelector('#close').addEventListener('click', ()=>{
        }
       });
 
-      function loader(){
-        document.querySelector('.loader-container').classList.add('fade-out');
-      }
+      // function loader(){
+      //   document.querySelector('.loader-container').classList.add('fade-out');
+      // }
 
-      function fadeOut(){
-        setInterval(loader, 3000);
-      }
+      // function fadeOut(){
+      //   setInterval(loader, 3000);
+      // }
 
-      window.onload = fadeOut;
+      // window.onload = fadeOut;
